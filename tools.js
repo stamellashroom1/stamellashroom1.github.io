61e48b8cbdfff2b4350ef6c7e4ad6097ac262daf
stamellashroom1 <alex@crpl.com.au>
2024-08-28 11:09:12 +1000

    tools improvements

--- tools.js
+++ tools.js
@@ -2,19 +2,30 @@
 (function () {
     const submit = document.getElementById("generalCalculatorSubmit");
     const answer = document.getElementById("calcAnswer");
+    const copy = document.getElementById("copyCalc");
+    const ANS = document.getElementById("calcANS");
+    copy.style.display = "none";
+    ANS.style.display = "none";
     answer.textContent = "";
+    answer.style.display = "none";
+    let numbers = [];
+    ANS.addEventListener("click", () => {
+        document.getElementById("generalCalculatorInput").value = document.getElementById("generalCalculatorInput").value + numbers[0];
+    });
+    copy.addEventListener("click", () => {
+        navigator.clipboard.writeText(numbers[0]);
+    });
     submit.addEventListener("click", () => {
         let input = String(document.getElementById("generalCalculatorInput").value);
         if (input) {
-            console.log(input);
+            // console.log(input);
 
             let signs = input.match(/[-\+\*\/^]/g);
-            console.log(`signs ${signs}`);
+            // console.log(`signs ${signs}`);
 
             let regex = /(\d+\.?\d*)/g;
-            let numbers = [];
             numbers = input.match(regex);
-            console.log(`numbers ${numbers}`);
+            // console.log(`numbers ${numbers}`);
 
             let signIndex;
             let signIndex1;
@@ -50,8 +61,11 @@
                 numbers.splice(signIndex, 2, completedOperation);
                 signs.splice(signIndex, 1);
             }
-            console.log(`Answer is ${numbers}`);
+            // console.log(`Answer is ${numbers}`);
+            answer.style.display = "inline-block";
             answer.textContent = "Answer is " + numbers;
+            ANS.style.display = "inline-block";
+            copy.style.display = "inline-block";
         }
     });
 })();
