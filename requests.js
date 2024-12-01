3c4b1a6b1d19e65e20758854c8b3521ef6ad91f6
stamellashroom1 <alex@crpl.com.au>
2024-12-01 12:39:51 +1100

    Update requests.js

--- requests.js
+++ requests.js
@@ -8,17 +8,7 @@ dateButton.addEventListener('click', async () => {
             throw new Error("Network response was not ok");
         }
         const data = await response.json();
-        printDate.textContent = `${data.message} at ${data.timestamp.toLocaleDateString('en-AU',
-            {
-                weekday: 'long',
-                year: 'numeric',
-                month: 'long',
-                day: 'numeric',
-                hour: '2-digit',
-                minute: '2-digit',
-                second: '2-digit'
-            }
-        )}`;
+        printDate.textContent = `${data.message} at ${data.timestamp.toDateString()}`;
     } catch (error) {
         console.error("There was a problem with the fetch operation:", error);
     }
