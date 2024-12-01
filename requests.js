0d7fa0b7a8ed59a9fa4c8f067ea0c7357ad6f932
stamellashroom1 <alex@crpl.com.au>
2024-12-01 12:23:17 +1100

    requests

---
+++ requests.js
@@ -0,0 +1,15 @@
+const dateButton = document.getElementById("getDate");
+const printDate = document.getElementById("date");
+
+dateButton.addEventListener('click', async () => {
+    try {
+        const response = await fetch("https://render-app-1-let5.onrender.com/test");
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        printDate.textContent = `${data.message} at ${data.timestamp}`;
+    } catch (error) {
+        console.error("There was a problem with the fetch operation:", error);
+    }
+});
