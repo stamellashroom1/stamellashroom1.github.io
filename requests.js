2e0135091d1e6078108ab4eb44577f4e4bb03556
stamellashroom1 <alex@crpl.com.au>
2025-11-15 18:23:19 +1100

    basic messaging

--- requests.js
+++ requests.js
@@ -1,6 +1,10 @@
 const dateButton = document.getElementById("getDate");
 const printDate = document.getElementById("date");
 
+const messages = document.getElementById("messages");
+const messageInput = document.getElementById("messageInput");
+const sendMessage = document.getElementById("sendMessage");
+
 dateButton.addEventListener('click', async () => {
     try {
         const response = await fetch("https://render-app-1-let5.onrender.com/test");
@@ -25,3 +29,99 @@ dateButton.addEventListener('click', async () => {
         console.error("There was a problem with the fetch operation:", error);
     }
 });
+
+let ws = null;
+
+function addMessage(message, css) {
+    const messageDiv = document.createElement('div');
+    messageDiv.textContent = message;
+    if (css) {
+        messageDiv.style = css;
+    }
+    messages.appendChild(messageDiv);
+    messages.scrollTop = messages.scrollHeight;
+}
+
+let nextCSS;
+
+function init() {
+    if (ws !== null) {
+        ws.close(1000, "close");
+        ws = null;
+    }
+    ws = new WebSocket("ws://127.0.0.1:3000");
+
+    ws.onmessage = function onMessage(event) {
+        const text = event.data;
+        if (text[0] === "?") {
+            nextCSS = text.slice(1, text.length);
+            console.log(nextCSS);
+        } else {
+            addMessage(text, nextCSS);
+            nextCSS = "";
+        }
+    }
+}
+
+init()
+
+sendMessage.addEventListener("click", () => {
+    message()
+});
+
+messageInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        e.preventDefault()
+        message()
+    }
+});
+
+function message() {
+    if (messageInput.value) {
+        switch (messageInput.value) {
+            case "/help":
+                addMessage(".", "color:white");
+                addMessage("/close: closes connection");
+                addMessage("/reset: resets connection");
+                addMessage("/open: opens connection if not already");
+                addMessage("/clear: clears messages on YOUR end");
+                addMessage("/help: displays this menu");
+                addMessage(".", "color:white");
+                break;
+            case "/clear":
+                clear()
+                break;
+            case "/reset":
+                init()
+                break;
+            case "/close":
+                close();
+                break;
+            case "/open":
+                if (ws !== null) {
+                    addMessage("Connection already open.");
+                } else {
+                    init();
+                }
+                break;
+            default:
+                try {
+                    ws.send(messageInput.value);
+                } catch (error) {
+                    console.log(`ws.send() failed: ${error}`);
+                }
+        }
+        messageInput.value = "";
+    }
+}
+
+function close() {
+    ws.close(1000, "exit");
+    ws = null;
+
+    addMessage("Connection closed.");
+}
+
+function clear() {
+    messages.textContent = "";
+}
